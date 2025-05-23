c44e417b66d7570c7db6cad2f5f32c359a509ee2
ashokkumar <ashkanduri@gmail.com>
2025-05-24 00:12:34 +0530

    make the changes

--- redis.sh
+++ redis.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
-
-#check if the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo "you are running with root access" | tee -a $LOG_FILE
-fi
-
-#validate functions take input as exit status for the command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is .... $G SUCCESSFUL $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is .... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+source ./common.sh
+root_setup
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabling default redis"
@@ -53,7 +21,4 @@ VALIDATE $? "enabling redis"
 systemctl start redis &>>$LOG_FILE
 VALIDATE $? "starting redis"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "script execution completed succesfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+time_setup
--- user.sh
+++ user.sh
@@ -1,85 +1,9 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+app_name=user
 
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "script started executing at: $(date)" | tee -a $LOG_FILE
-
-#check if the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR:: please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo "you are running with root access" | tee -a $LOG_FILE
-fi
-
-#validate functions take input as exit status for the command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is .... $G SUCCESSFUL $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is .... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "disabling default nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "enabling nodejs:20"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "installing nodejs:20"
-
-id roboshop
-if [ $? -ne 0 ]
-then 
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "creating roboshop system user"
-else 
-    echo -e "system user roboshop already created... $Y SKIPPING $N"
-fi
-
-mkdir -p /app
-VALIDATE $? "creating app directory"
-
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading user" 
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/user.zip &>>$LOG_FILE
-VALIDATE $? "unzipping user"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "installing dependencies"
-
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
-VALIDATE $? "copying user service"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "daemon reload"
-
-systemctl enable user &>>$LOG_FILE
-VALIDATE $? "enabling user"
-
-systemctl start user 
-VALIDATE $? "starting user"
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "script execution completed succesfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+app_setup
+nodejs_setup
+systemd_setup
+time_setup
